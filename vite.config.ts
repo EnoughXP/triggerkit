369c6132cb69182d9638ff35430dfbea2a1be166
Cpt_Crunch <jasong84@gmail.com>
2025-02-21 16:33:23 -0600

    WIP: Tests

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**'],
+    maxWorkers: 1,
+    poolOptions: {
+      threads: {
+        maxThreads: 1,
+        minThreads: 1
+      }
+    },
+    testTimeout: 10000,
+    teardownTimeout: 1000
   }
 });
